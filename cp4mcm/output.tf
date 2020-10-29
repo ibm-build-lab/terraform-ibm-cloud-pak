a03511807f5e830c2a930bc984b7edabbb495325
Johandry Amador <Johandry.Amador@ibm.com>
2020-10-29 09:35:10 -0700

    Fix output to use data external

--- cp4mcm/output.tf
+++ cp4mcm/output.tf
@@ -7,10 +7,12 @@ output "endpoint" {
 }
 
 output "user" {
-  value = var.enable && length(data.kubernetes_secret.mcm_credentials) > 0 ? data.kubernetes_secret.mcm_credentials.0.data.admin_username : ""
+  // value = var.enable && length(data.kubernetes_secret.mcm_credentials) > 0 ? data.kubernetes_secret.mcm_credentials.0.data.admin_username : ""
+  value = var.enable && length(data.external.kubectl_get_mcm_admin_username) > 0 ? data.external.kubectl_get_mcm_admin_username.0.results.username : ""
 }
 
 output "password" {
-  value = var.enable && length(data.kubernetes_secret.mcm_credentials) > 0 ? data.kubernetes_secret.mcm_credentials.0.data.admin_password : ""
+  // value = var.enable && length(data.kubernetes_secret.mcm_credentials) > 0 ? data.kubernetes_secret.mcm_credentials.0.data.admin_password : ""
+  value = var.enable && length(data.external.kubectl_get_mcm_admin_password) > 0 ? data.external.kubectl_get_mcm_admin_password.0.results.password : ""
 }
 
