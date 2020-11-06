6bfbd6a072973fe252aa5e98da34debcfa55bf89
Johandry Amador <Johandry.Amador@ibm.com>
2020-11-05 23:44:59 -0800

    decrypt the user & passwd params

--- cp4mcm/scripts/get_endpoints.sh
+++ cp4mcm/scripts/get_endpoints.sh
@@ -6,8 +6,8 @@ NAMESPACE="ibm-common-services"
 eval "$(jq -r '@sh "export KUBECONFIG=\(.kubeconfig)"')"
 
 credentials=$(kubectl get secret platform-auth-idp-credentials -n ${NAMESPACE} -o jsonpath='{.data}')
-username=$(echo $credentials | jq -r .admin_username)
-password=$(echo $credentials | jq -r .admin_password)
+username=$(echo $credentials | jq -r .admin_username | base64 -d)
+password=$(echo $credentials | jq -r .admin_password | base64 -d)
 host=$(kubectl get route cp-console -n ${NAMESPACE} -o jsonpath='{.spec.host}')
 
 jq -n \
