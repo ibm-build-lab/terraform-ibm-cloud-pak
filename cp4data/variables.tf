2fda0842817a2c558510cef44764634da02a40ec
Johandry Amador <Johandry.Amador@ibm.com>
2021-01-14 17:16:57 -0800

    alternative values to docker vars in comments

--- cp4data/variables.tf
+++ cp4data/variables.tf
@@ -169,11 +169,9 @@ locals {
   namespace                = "cloudpak4data"
   entitled_registry        = "cp.icr.io"
   entitled_registry_user   = "cp"
-  docker_registry          = "cp.icr.io/cp/cpd"
-  docker_username          = "cp" // "ekey"
+  docker_registry          = "cp.icr.io/cp/cpd" // Staging: "cp.stg.icr.io/cp/cpd"
+  docker_username          = "cp"               // "ekey"
   entitled_registry_key    = chomp(var.entitled_registry_key)
   openshift_version_regex  = regex("(\\d+).(\\d+)(.\\d+)*(_openshift)*", var.openshift_version)
   openshift_version_number = local.openshift_version_regex[3] == "_openshift" ? tonumber("${local.openshift_version_regex[0]}.${local.openshift_version_regex[1]}") : 0
-  // For Staging, use:
-  // docker_registry          = "cp.stg.icr.io/cp/cpd"
 }
