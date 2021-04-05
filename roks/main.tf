554fb1384f46b434a9158da7d33eb7dd27f39f7b
Tim Robinson <timroster@gmail.com>
2021-04-05 16:33:54 -0700

    implement region as local

--- roks/main.tf
+++ roks/main.tf
@@ -1,5 +1,6 @@
 data "ibm_resource_group" "group" {
   name = var.resource_group
+  region     = local.region
 }
 
 // resource "ibm_resource_group" "group" {
--- roks/variables.tf
+++ roks/variables.tf
@@ -105,5 +105,6 @@ locals {
   max_size        = length(var.vpc_zone_names)
   cluster_name_id = var.on_vpc ? join("", ibm_container_vpc_cluster.cluster.*.id) : join("", ibm_container_cluster.cluster.*.id)
   roks_version    = format("%s_openshift", split("_", var.roks_version)[0])
+  region          = "us-east"
 }
 
