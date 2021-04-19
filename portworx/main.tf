eef4ad672caf3d7df0d4ad915cce21adda2f1f04
Eric Cambel <eric.cambel@ibm.com>
2021-04-19 13:21:10 -0400

    Added check on install_storage

--- portworx/main.tf
+++ portworx/main.tf
@@ -22,7 +22,7 @@ px_storage_cluster   = "${var.base_name}-px-storage-cluster"
 }
 
 resource "null_resource" "create_storage" {
-  count = var.enable ? 1 : 0
+  count = var.enable && var.install_storage ? 1 : 0
 
   provisioner "local-exec" {
     command     = "./install_portworx_storage.sh"
@@ -40,6 +40,7 @@ resource "null_resource" "create_storage" {
 }
 
 resource "ibm_resource_instance" "portworx_instance" {
+  count = var.enable ? 1 : 0
   name              = local.px_service_name
   location          = var.dc_region # "us-south", "us-east", "eu-gb", "eu-de", "jp-tok", "au-syd", etc..
   resource_group_id = data.ibm_resource_group.group.id
