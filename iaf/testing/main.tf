3c84b7f53c87d814e28372f87c52683155506f55
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-04-28 12:21:07 -0500

    Update main.tf

--- iaf/testing/main.tf
+++ iaf/testing/main.tf
@@ -9,7 +9,15 @@ data "ibm_resource_group" "group" {
   name = var.resource_group
 }
 
+resource "null_resource" "mkdir_kubeconfig_dir" {
+  triggers = { always_run = timestamp() }
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.config_dir}"
+  }
+}
+
 data "ibm_container_cluster_config" "cluster_config" {
+  depends_on = [null_resource.mkdir_kubeconfig_dir]
   cluster_name_id   = var.cluster_id
   resource_group_id = data.ibm_resource_group.group.id
   config_dir        = var.config_dir
