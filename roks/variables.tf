b71bc1c72b53e40b8ac4ab09a92fcb98087c725a
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-05-06 17:20:52 -0500

    Update variables.tf

--- roks/variables.tf
+++ roks/variables.tf
@@ -86,7 +86,7 @@ variable "force_delete_storage" {
 }
 
 variable "datacenter" {
-  description = "On IBM Cloud Classic, this is the datacenter where the cluster will be provisioned. List all available datacenters/zones with: ibmcloud ks zone ls --provider classic"
+  description = "On IBM Cloud Classic, this is the datacenter where the cluster will be provisioned. List all available datacenters/zones with: `ibmcloud ks zone ls --provider classic`"
   default     = ""
 }
 
