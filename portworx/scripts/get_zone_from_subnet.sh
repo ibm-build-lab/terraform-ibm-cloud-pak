079f592c6c68f04f62ad075cee0d9cb1cf93ba55
Eric Cambel <eric.cambel@ibm.com>
2021-05-20 18:35:05 -0400

    initial testing of getting a zone

---
+++ portworx/scripts/get_zone_from_subnet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+response=`curl -s -X GET "https://${REGION}.iaas.cloud.ibm.com/v1/subnets/${IDENTIFIER}?version=2021-05-18&generation=2" -H "Authorization: ${TOKEN}"`
+
+zone_name=$(echo ${response} | jq -r '.zone.name')
+
+jq -n --arg zone_name "${zone_name}" '{ "zone_name": $zone_name }'
