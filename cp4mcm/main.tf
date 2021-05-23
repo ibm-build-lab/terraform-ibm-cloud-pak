bc2c5c661f21303f55e96ec242d29cae3d157586
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-05-23 13:43:05 -0500

    Update main.tf

--- cp4mcm/main.tf
+++ cp4mcm/main.tf
@@ -36,7 +36,7 @@ resource "null_resource" "install_cp4mcm" {
   }
 
   provisioner "local-exec" {
-    depends_on = [local.roks_is_ready]
+    depends_on = [var.roks_is_ready]
     
     command = "${path.module}/scripts/install_cp4mcm.sh"
 
