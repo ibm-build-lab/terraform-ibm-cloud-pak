3cdeca6ddb44a95848483bd070b9e8d4ecfa5eda
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-05-26 18:11:33 -0500

    Update install_cp4mcm.sh

--- cp4mcm/scripts/install_cp4mcm.sh
+++ cp4mcm/scripts/install_cp4mcm.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+ibmcloud config --check-version=false
+ibmcloud login -apikey ${IC_API_KEY}
+ibmcloud ks cluster config -c ${MCM_CLUSTER} --admin
+
 echo "Creating namespace ${MCM_NAMESPACE}"
 kubectl create namespace ${MCM_NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
 
