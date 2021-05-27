f130ebe16470f3d35b7eafb6e97f5a28d8c4d687
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-05-27 10:36:40 -0500

    Update install_cp4mcm.sh

--- cp4mcm/scripts/install_cp4mcm.sh
+++ cp4mcm/scripts/install_cp4mcm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ibmcloud config --check-version=false
-ibmcloud login -apikey ${IC_API_KEY}
+#ibmcloud config --check-version=false
+#ibmcloud login -apikey ${IC_API_KEY}
 #ibmcloud ks cluster config -c ${MCM_CLUSTER} --admin
 
 echo "Creating namespace ${MCM_NAMESPACE}"
