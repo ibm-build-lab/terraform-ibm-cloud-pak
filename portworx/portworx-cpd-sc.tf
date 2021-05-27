a5c4edece63f90fab1847b27cb8b5f19e7b99d9e
Eric Cambel <eric.cambel@ibm.com>
2021-05-27 17:48:10 -0400

    Added enable vars to px resources

--- portworx/portworx-cpd-sc.tf
+++ portworx/portworx-cpd-sc.tf
@@ -4,6 +4,8 @@
 
 # IBM Cloud Portworx creates a default db2 sc that needs to be deleted to avoid a naming conflict with px-sc.sh
 resource "null_resource" "delete_db2_sc" {
+  count = var.enable ? 1 : 0
+
   depends_on = [ibm_resource_instance.portworx]
   
   provisioner "local-exec" {
@@ -18,6 +20,8 @@ resource "null_resource" "delete_db2_sc" {
 
 # px-sc.sh comes from https://github.ibm.com/PrivateCloud-analytics/portworx-util/blob/px-2.5.5/cpd-portworx/px-2.6.2.0/px-install-4.x/px-sc.sh
 resource "null_resource" "px_sc" {
+  count = var.enable ? 1 : 0
+
   depends_on = [ibm_resource_instance.portworx, null_resource.delete_db2_sc]
   
   provisioner "local-exec" {
