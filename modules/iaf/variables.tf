f75732d2fb71d58a511fbf28eae5783042fcfa31
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-07-08 11:51:18 -0500

    Update variables.tf

--- modules/iaf/variables.tf
+++ modules/iaf/variables.tf
@@ -59,7 +59,5 @@ locals {
   entitled_registry        = "cp.icr.io"
   entitled_registry_user   = "cp"
   entitled_registry_key    = chomp(var.entitled_registry_key)
-  ibmcloud_api_key               = chomp(var.ibmcloud_api_key)
-  openshift_version_regex  = regex("(\\d+).(\\d+)(.\\d+)*(_openshift)*", var.openshift_version)
-  openshift_version_number = local.openshift_version_regex[3] == "_openshift" ? tonumber("${local.openshift_version_regex[0]}.${local.openshift_version_regex[1]}") : 0
+  ibmcloud_api_key         = chomp(var.ibmcloud_api_key)
 }
