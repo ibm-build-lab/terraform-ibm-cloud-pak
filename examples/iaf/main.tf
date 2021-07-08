28fb6fcbb5eb7b21ae8e3bbfd7cb221d09fb82f5
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-07-08 12:52:51 -0500

    Update main.tf

--- examples/iaf/main.tf
+++ examples/iaf/main.tf
@@ -32,13 +32,15 @@ module "iaf" {
   cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
   cluster_name_id     = var.cluster_id
   on_vpc              = var.on_vpc
+  region              = var.region
+  resource_group      = var.resource_group
 
   // IBM Cloud API Key
   ibmcloud_api_key          = var.ibmcloud_api_key
 
   // Entitled Registry parameters:
   // 1. Get the entitlement key from: https://myibm.ibm.com/products-services/containerlibrary
-  // 2. Save the key to a file, update the file path in the entitled_registry_key parameter
+  // 2. Save the key to a file, or list here
   entitled_registry_key        = length(var.entitled_registry_key) > 0 ? var.entitled_registry_key : file(local.entitled_registry_key_file)
   entitled_registry_user_email = var.entitled_registry_user_email
 }
