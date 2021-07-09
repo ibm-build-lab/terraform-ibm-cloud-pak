51966af2d7e45bca96d47c457f7b10e6d7f291d6
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-07-09 16:38:34 -0500

    Update output.tf

--- examples/roks/output.tf
+++ examples/roks/output.tf
@@ -14,11 +14,3 @@ output "vlan_number" {
   value = module.cluster.vlan_number
 }
 
-// Kubeconfig downloaded by this module
-output "config_file_path" {
-  value = data.ibm_container_cluster_config.cluster_config.config_file_path
-}
-  
-output "cluster_config" {
-  value = data.ibm_container_cluster_config.cluster_config
-}
