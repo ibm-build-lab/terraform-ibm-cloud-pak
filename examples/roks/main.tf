1b631b035945860a7981640d73b3e7faf5891975
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-07-09 12:43:03 -0500

    Update main.tf

--- examples/roks/main.tf
+++ examples/roks/main.tf
@@ -42,28 +42,3 @@ module "cluster" {
   vpc_zone_names = var.vpc_zone_names
 }
 
-// Test Output Parameters
-
-output "endpoint" {
-  value = module.cluster.endpoint
-}
-
-output "id" {
-  value = module.cluster.id
-}
-
-output "name" {
-  value = module.cluster.name
-}
-
-// output "config" {
-//   value = module.cluster.config
-// }
-
-// output "config_file_path" {
-//   value = data.ibm_container_cluster_config.cluster_config.config_file_path
-// }
-
-output "vlan_number" {
-  value = module.cluster.vlan_number
-}
