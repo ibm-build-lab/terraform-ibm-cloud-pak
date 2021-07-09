9d16ad2e410e4cb18ecbb94b80a486d40e61f724
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-07-09 14:13:34 -0500

    Update kubernetes.tf

--- examples/roks/kubernetes.tf
+++ examples/roks/kubernetes.tf
@@ -1,7 +1,7 @@
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id   = local.enable ? module.cluster.id : local.cluster_id
   resource_group_id = module.cluster.resource_group.id
-  config_dir        = var.config_dir
+  config_dir        = local.config_dir
 }
 
 
