126535b7283b3a7318147e6efef1f047215dba23
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-07-09 15:46:17 -0500

    Update main.tf

--- examples/roks/main.tf
+++ examples/roks/main.tf
@@ -4,9 +4,13 @@ provider "ibm" {
   region     = var.region
 }
 
+provider "kubernetes" {
+  config_path = local.config_dir
+}
+
 module "cluster" {
   source = "../../modules/roks"
-  enable = local.enable
+  enable = local.enable_cluster
   on_vpc = var.on_vpc
 
   // General
@@ -18,20 +22,14 @@ module "cluster" {
   entitlement    = var.entitlement
   force_delete_storage = var.force_delete_storage
 
-  // Parameters for Kubernetes Config
-  // download_config = length(var.config_dir) > 0
-  // config_dir      = var.config_dir
-  // config_admin    = false
-  // config_network  = false
-
-  // Temporary, until the issue with the API permissions issues is fixed
-  private_vlan_number = var.private_vlan_number
-  public_vlan_number  = var.public_vlan_number
-
   // Parameters for the Workers
   flavors        = var.flavors
   workers_count  = var.workers_count
+  // Classic only
   datacenter     = var.datacenter
+  private_vlan_number = var.private_vlan_number
+  public_vlan_number  = var.public_vlan_number
+  // VPC only
   vpc_zone_names = var.vpc_zone_names
 }
 
