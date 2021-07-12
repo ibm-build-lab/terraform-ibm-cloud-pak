72f02c8682dffa07d82ccb25dd6031e6fb2725c2
Eric Cambel <eric.cambel@ibm.com>
2021-07-12 13:28:01 -0400

    removed comments

--- modules/cp4aiops/scripts/get_endpoints.sh
+++ modules/cp4aiops/scripts/get_endpoints.sh
@@ -21,10 +21,6 @@ results() {
   exit 0
 }
 
-# POD=$(kubectl get pods -n cpd-meta-ops | grep ibm-cp-data-operator | awk '{print $1}')
-# control_plane_log=$(kubectl logs -n cpd-meta-ops $POD | sed 's/[[:cntrl:]]\[[0-9;]*m//g' | tail -20)
-# address=$(echo $control_plane_log | sed -n 's#.*\(https*://[^"]*\).*#\1#p')
-
 route=$(oc get route -n ${NAMESPACE} cpd -o jsonpath=‘{.spec.host}’)
 pass=$(oc -n ibm-common-services get secret platform-auth-idp-credentials -o jsonpath='{.data.admin_password}' | base64 -d && echo)
 user=$(oc -n ibm-common-services get secret platform-auth-idp-credentials -o jsonpath='{.data.admin_username}' | base64 -d && echo)
