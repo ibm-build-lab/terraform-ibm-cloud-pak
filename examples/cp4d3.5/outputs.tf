ccb2209e84035d101aa7c91974fae176a7d0a4f4
Eric Cambel <eric.cambel@ibm.com>
2021-07-20 14:17:54 -0400

    added cp4d3.5 example

---
+++ examples/cp4d3.5/outputs.tf
@@ -0,0 +1,20 @@
+// CP4DATA output parameters
+output "cpd_url" {
+  description = "Access your Cloud Pak for Data deployment at this URL."
+  value = "https://${var.cpd_project_name}-cpd-${var.cpd_project_name}.${var.on_vpc ? join("", ibm_container_vpc_cluster.cluster.*.ingress_hostname) 
+  : join("", ibm_container_cluster.cluster.*.ingress_hostname)}"
+}
+
+output "cpd_user" {
+  description = "Username for your Cloud Pak for Data deployment."
+  value = "admin"
+}
+
+output "cpd_pass" {
+  description = "Password for your Cloud Pak for Data deployment."
+  value = "password"
+}
+
+output "namespace" {
+  value = var.cpd_project_name
+}
