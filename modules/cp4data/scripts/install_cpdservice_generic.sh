3f185e7573eb9df03b654a6945201ce63286e8c9
cambelem <ecambel4@yahoo.com>
2021-08-18 08:44:45 -0400

    Added fix for failing cpd3.5 services (#46)
    Co-authored-by: Eric Cambel <eric.cambel@ibm.com>

--- modules/cp4data/scripts/install_cpdservice_generic.sh
+++ modules/cp4data/scripts/install_cpdservice_generic.sh
@@ -13,9 +13,11 @@ if ! sed -e "s/SERVICE/${SERVICE}/g" -e "s/STORAGECLASS/${STORAGECLASS}/g" -e "s
   exit 1
 fi
 
-SLEEP_TIME="60"
-RUN_LIMIT=200
-i=0
+SLEEP_TIME="60" #  1 min
+SERVICE_DELETION_TIMEOUT_LIMIT=10 # minutes
+RUN_LIMIT=4 # Runs '5' times
+service_timeout_count=0
+run_limit_count=0
 
 while true; do
   if ! STATUS_LONG=$(oc -n ${NAMESPACE} get cpdservice ${SERVICE}-cpdservice --output=json | jq -c -r '.status'); then
@@ -26,23 +28,51 @@ while true; do
   echo $STATUS_LONG
   STATUS=$(echo $STATUS_LONG | jq -c -r '.status')
 
+  # If the service has been restarted RUN_LIMIT times, it will quit and time out.
+  if [ "$run_limit_count" -eq "$RUN_LIMIT" ]; then
+    echo 'Timed out'
+    exit 1
+  fi
+
   if [ "$STATUS" == "Ready" ]; then
     break
   fi
   
   if [ "$STATUS" == "Failed" ]; then
-    echo '=== Installation has failed ==='
-    exit 1
+    echo "=== ${SERVICE} Installation has failed ==="
+
+    echo "Deleting and retrying ${SERVICE} installation"
+    oc -n ${NAMESPACE} delete cpdservice ${SERVICE}-cpdservice
+
+    # Set counter for potential run away deletion timeout
+    service_deletion_timeout_count=0
+    while true; do
+
+      # If the deletion takes too long to delete (SERVICE_TIMEOUT_LIMIT), it will fail and exit
+      if [ "$service_deletion_timeout_count" -eq "$SERVICE_DELETION_TIMEOUT_LIMIT" ]; then
+        echo "=== ${SERVICE} Installation has failed ==="
+        echo "Deleting ${SERVICE} from ${NAMESPACE} has failed."
+        exit 1
+      fi
+
+      echo "Waiting for ${SERVICE} to finish deleting..."
+      if [ -z $(oc -n ${NAMESPACE} get cpdservice ${SERVICE}-cpdservice | awk '{print $1}') ]; then
+        break
+      fi
+
+      sleep $SLEEP_TIME
+      (( service_deletion_timeout_count++ ))
+    done
+
+    echo "Recreating ${SERVICE} service..."
+    sed -e "s/SERVICE/${SERVICE}/g" -e "s/STORAGECLASS/${STORAGECLASS}/g" -e "s/OVERRIDE/\"${OVERRIDE}\"/g" ../templates/metaoperator.cpd.ibm.com_v1_cpdservice_cr.yaml | oc -n ${NAMESPACE} apply -f -
+
+    (( run_limit_count++ ))
   fi
   
   echo "Sleeping $SLEEP_TIME seconds..."
   sleep $SLEEP_TIME
   
-  (( i++ ))
-  if [ "$i" -eq "$RUN_LIMIT" ]; then
-    echo 'Timed out'
-    exit 1
-  fi
 done
 
 echo "=== ${SERVICE} installed ==="
