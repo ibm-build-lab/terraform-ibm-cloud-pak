9ba1406da134b52d2509f48a977a09a44198daff
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-08-25 12:49:37 -0500

    Update variables.tf

--- modules/ldap/variables.tf
+++ modules/ldap/variables.tf
@@ -50,7 +50,7 @@ variable "memory" {
 
 variable "disks" {
     default = null
-    description = "The numeric disk sizes in GBs for the instance's block device and disk image settings."
+    description = "Array of numeric disk sizes in GBs for the instance's block device and disk image settings. Example: [25] or [25, 10, 20]"
 }
 
 variable "hostname" {
