ceb5ffa79a03b2b1b12d0a4ec1744e125a540b1f
Jeremy Menke <87037301+jeremymenke@users.noreply.github.com>
2021-08-31 14:58:34 -0400

    cloudctl-linux-amd64 - data refinery

--- modules/cp4data_4.0/scripts/install-data-refinery.sh
+++ modules/cp4data_4.0/scripts/install-data-refinery.sh
@@ -8,23 +8,29 @@ wget https://raw.githubusercontent.com/IBM/cloud-pak/master/repo/case/ibm-datare
 
 CASE_PACKAGE_NAME="ibm-datarefinery-1.0.0.tgz"
 
-cloudctl case launch --case ./${CASE_PACKAGE_NAME} \
+./cloudctl-linux-amd64 case launch --case ./${CASE_PACKAGE_NAME} \
     --tolerance 1 --namespace ${OP_NAMESPACE}         \
     --action installOperator                        \
     --inventory datarefinerySetup
 
 # Checking if the data-refinery operator pods are ready and running. 
 # checking status of ibm-cpd-datarefinery-operator
-./pod-status-check.sh ibm-cpd-datarefinery-operator ${OP_NAMESPACE}
+# ./pod-status-check.sh ibm-cpd-datarefinery-operator ${OP_NAMESPACE}
 
 # switch to zen namespace
 oc project ${NAMESPACE}
+
+echo '*** cd ../files'
+cd ../files
 
 # Create data-refinery CR: 
 
 echo '*** executing **** oc create -f data-refinery-cr.yaml'
 result=$(oc create -f data-refinery-cr.yaml)
 echo $result
+
+echo '*** cd ../scripts'
+cd ../scripts
 
 # check the data-refinery cr status
 ./check-cr-status.sh Datarefinery datarefinery-cr ${NAMESPACE} datarefineryStatus
