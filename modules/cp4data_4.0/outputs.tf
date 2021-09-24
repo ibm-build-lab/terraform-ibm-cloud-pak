3c2481cfd283471098b24930fddacf327f438d5b
jeremymenke <87037301+jeremymenke@users.noreply.github.com>
2021-09-24 16:35:37 -0400

    Update outputs.

--- modules/cp4data_4.0/outputs.tf
+++ modules/cp4data_4.0/outputs.tf
@@ -1,14 +1,21 @@
 output "endpoint" {
+  depends_on = [
+    data.external.get_endpoints,
+  ]
   value = var.enable && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.endpoint : ""
 }
 
 output "user" {
-  // value = var.enable && length(data.kubernetes_secret.mcm_credentials) > 0 ? data.kubernetes_secret.mcm_credentials.0.data.admin_username : ""
+  depends_on = [
+    data.external.get_endpoints,
+  ]
   value = var.enable && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.username : ""
 }
 
 output "password" {
-  // value = var.enable && length(data.kubernetes_secret.mcm_credentials) > 0 ? data.kubernetes_secret.mcm_credentials.0.data.admin_password : ""
+  depends_on = [
+    data.external.get_endpoints,
+  ]
   value = var.enable && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.password : ""
 }
 
