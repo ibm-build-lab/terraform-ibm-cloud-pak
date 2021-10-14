ea166c33a56bdb43b48f847d8226fc338bea32ce
Tim Robinson <timroster@gmail.com>
2021-10-14 07:48:29 -0700

    update reencrypt for new ca cert (#63)
    Signed-off-by: Tim Robinson <timroster@gmail.com>

--- modules/cp4data_4.0/scripts/reencrypt_route.sh
+++ modules/cp4data_4.0/scripts/reencrypt_route.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 NAMESPACE=$1
-nginxpod=$(oc -n $NAMESPACE get pod -l component=ibm-nginx -o jsonpath='{.items[0].metadata.name}')
-oc -n $NAMESPACE cp $nginxpod:/etc/nginx/config/ssl/cert.crt /tmp/cert.crt || exit 1
-oc -n $NAMESPACE delete route ${NAMESPACE}-cpd
-oc -n $NAMESPACE create route reencrypt ${NAMESPACE}-cpd --service=ibm-nginx-svc --port=ibm-nginx-https-port --dest-ca-cert=/cert.crt
+oc -n $NAMESPACE extract secret/ibm-nginx-internal-tls-ca --keys=cert.crt --to=/tmp > /dev/null
+oc -n $NAMESPACE delete route cpd
+oc -n $NAMESPACE create route reencrypt cpd --service=ibm-nginx-svc --port=ibm-nginx-https-port --dest-ca-cert=/tmp/cert.crt
 rm /tmp/cert.crt
