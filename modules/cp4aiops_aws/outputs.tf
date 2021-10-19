171369db741b3c561c7de3e926e063da8d2f64a9
cambelem <ecambel4@yahoo.com>
2021-10-19 11:36:14 -0400

    Cp4aiops aws (#59)
    * cp4aiops aws module
    
    Co-authored-by: Eric Cambel <eric.cambel@ibm.com>

---
+++ modules/cp4aiops_aws/outputs.tf
@@ -0,0 +1,20 @@
+output "cp4aiops_endpoint" {
+  depends_on = [
+    data.external.get_endpoints,
+  ]
+  value = var.enable && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.endpoint : ""
+}
+
+output "cp4aiops_user" {
+  depends_on = [
+    data.external.get_endpoints,
+  ]
+  value = var.enable && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.username : ""
+}
+
+output "cp4aiops_password" {
+  depends_on = [
+    data.external.get_endpoints,
+  ]
+  value = var.enable && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.password : ""
+}
