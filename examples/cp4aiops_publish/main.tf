0ba0bf28fbc0eb38986feb978e31f45e85eff78a
Joel G <32661507+jgod1360@users.noreply.github.com>
2021-10-19 13:46:00 -0400

    Update main.tf

--- examples/cp4aiops_publish/main.tf
+++ examples/cp4aiops_publish/main.tf
@@ -1,10 +1,11 @@
-// Requirements:
 terraform {
   required_version = ">=0.13"
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
       version    = "~> 1.12"
+      region     = var.region
+      ibmcloud_api_key = var.ibmcloud_api_key
     }
   }
 }
