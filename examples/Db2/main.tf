b06e107902c45354695438804161597e7872a141
jgod360 <joel.goddot@gmail.com>
2022-01-28 15:58:06 -0500

    issue_223 db2 example

--- examples/Db2/main.tf
+++ examples/Db2/main.tf
@@ -1,5 +1,6 @@
 provider "ibm" {
-  version          = "~> 1.12"
+  region           = var.region
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 data "ibm_resource_group" "group" {
@@ -15,8 +16,8 @@ resource "null_resource" "mkdir_kubeconfig_dir" {
 }
 
 data "ibm_container_cluster_config" "cluster_config" {
-  depends_on = [null_resource.mkdir_kubeconfig_dir]
-  cluster_name_id   = var.cluster_name_or_id
+  depends_on        = [null_resource.mkdir_kubeconfig_dir]
+  cluster_name_id   = var.cluster_id
   resource_group_id = data.ibm_resource_group.group.id
   download          = true
   config_dir        = local.cluster_config_path
@@ -25,20 +26,21 @@ data "ibm_container_cluster_config" "cluster_config" {
 }
 
 module "Db2" {
-  source = "../../modules/Db2"
+  source = "..\/..\/modules\/Db2"
   enable = true
 
   # ----- Cluster -----
-  KUBECONFIG = data.ibm_container_cluster_config.cluster_config.config_file_path
+  cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
 
   # ----- Platform -----
-  DB2_PROJECT_NAME        = local.db2_project_name
-  DB2_ADMIN_USER_NAME     = local.db2_admin_user_name
-  DB2_ADMIN_USER_PASSWORD = local.db2_admin_user_password
+  db2_project_name        = var.db2_project_name
+  db2_admin_username      = var.db2_admin_username
+  db2_admin_user_password = var.db2_admin_user_password
+  db2_standard_license_key = var.db2_standard_license_key
 
   # ------ Docker Information ----------
-  ENTITLED_REGISTRY_KEY           = var.entitlement_key
-  ENTITLEMENT_REGISTRY_USER_EMAIL = var.entitled_registry_user_email
-  DOCKER_SERVER                   = local.docker_server
-  DOCKER_USERNAME                 = local.docker_username
+  entitlement_key           = var.entitlement_key
+  entitled_registry_user_email = var.entitled_registry_user_email
+//  docker_server                   = local.docker_server
+//  docker_username                 = local.docker_username
 }
--- examples/Db2/variables.tf
+++ examples/Db2/variables.tf
@@ -1,6 +1,14 @@
-variable "cluster_name_or_id" {
+variable "cluster_id" {
   default     = ""
   description = "Enter your cluster id or name to install the Cloud Pak. Leave blank to provision a new Openshift cluster."
+}
+
+variable "ibmcloud_api_key" {
+  description = "IBMCloud API key (https://cloud.ibm.com/docs/account?topic=account-userapikey#create_user_key)"
+}
+
+variable "region" {
+  description = "Region that cluster resides in"
 }
 
 variable "resource_group" {
@@ -8,7 +16,7 @@ variable "resource_group" {
   description = "Resource group name where the cluster will be hosted."
 }
 variable "entitled_registry_user_email" {
-  type = string
+  type        = string
   description = "Email address of the user owner of the Entitled Registry Key"
 }
 
@@ -17,26 +25,62 @@ variable "entitlement_key" {
   description = "Do you have a Cloud Pak for Business Automation Entitlement Registry key? If not, Get the entitlement key from https://myibm.ibm.com/products-services/containerlibrary"
 }
 
+# --------- DB2 SETTINGS ----------
 variable "enable" {
   default = true
-  description = "If set to true installs Cloud-Pak for Business Automation on the given cluster"
+  description = "If set to true, it will install DB2 on the given cluster"
 }
 
-locals {
-  db2_admin_user_password  = "passw0rd"
-  db2_standard_license_key = "W0xpY2Vuc2VDZXJ0aWZpY2F0ZV0KQ2hlY2tTdW09Q0FBODlCOTA0QzU3RTY2OTU1RjJDQTY4MzlCRTZCOTMKVGltZVN0YW1wPTE1NjU3MjM5MDIKUGFzc3dvcmRWZXJzaW9uPTQKVmVuZG9yTmFtZT1JQk0gVG9yb250byBMYWIKVmVuZG9yUGFzc3dvcmQ9N3Y4cDRmcTJkdGZwYwpWZW5kb3JJRD01ZmJlZTBlZTZmZWIuMDIuMDkuMTUuMGYuNDguMDAuMDAuMDAKUHJvZHVjdE5hbWU9REIyIFN0YW5kYXJkIEVkaXRpb24KUHJvZHVjdElEPTE0MDUKUHJvZHVjdFZlcnNpb249MTEuNQpQcm9kdWN0UGFzc3dvcmQ9MzR2cnc1MmQyYmQyNGd0NWFmNHU4Y2M0ClByb2R1Y3RBbm5vdGF0aW9uPTEyNyAxNDMgMjU1IDI1NSA5NCAyNTUgMSAwIDAgMC0yNzsjMCAxMjggMTYgMCAwCkFkZGl0aW9uYWxMaWNlbnNlRGF0YT0KTGljZW5zZVN0eWxlPW5vZGVsb2NrZWQKTGljZW5zZVN0YXJ0RGF0ZT0wOC8xMy8yMDE5CkxpY2Vuc2VEdXJhdGlvbj02NzE2CkxpY2Vuc2VFbmREYXRlPTEyLzMxLzIwMzcKTGljZW5zZUNvdW50PTEKTXVsdGlVc2VSdWxlcz0KUmVnaXN0cmF0aW9uTGV2ZWw9MwpUcnlBbmRCdXk9Tm8KU29mdFN0b3A9Tm8KQnVuZGxlPU5vCkN1c3RvbUF0dHJpYnV0ZTE9Tm8KQ3VzdG9tQXR0cmlidXRlMj1ObwpDdXN0b21BdHRyaWJ1dGUzPU5vClN1YkNhcGFjaXR5RWxpZ2libGVQcm9kdWN0PU5vClRhcmdldFR5cGU9QU5ZClRhcmdldFR5cGVOYW1lPU9wZW4gVGFyZ2V0ClRhcmdldElEPUFOWQpFeHRlbmRlZFRhcmdldFR5cGU9CkV4dGVuZGVkVGFyZ2V0SUQ9ClNlcmlhbE51bWJlcj0KVXBncmFkZT1ObwpJbnN0YWxsUHJvZ3JhbT0KQ2FwYWNpdHlUeXBlPQpNYXhPZmZsaW5lUGVyaW9kPQpEZXJpdmVkTGljZW5zZVN0eWxlPQpEZXJpdmVkTGljZW5zZVN0YXJ0RGF0ZT0KRGVyaXZlZExpY2Vuc2VFbmREYXRlPQpEZXJpdmVkTGljZW5zZUFnZ3JlZ2F0ZUR1cmF0aW9uPQo="
-  db2_admin_user_name           = "db2inst1"
-  db2_project_name              = "ibm-db2"
+ variable "db2_project_name" {
+   default = "ibm-db2"
+   description = "The namespace/project for Db2"
+ }
+variable "db2_admin_user_password" {
+  description = "Db2 admin user password defined in LDAP"
 }
 
+variable "db2_admin_username" {
+  default = "db2inst1"
+  description = "Db2 admin username defined in LDAP"
+}
+
+//variable "db2_host_name" {
+//  description = "Host name of Db2 instance"
+//}
+//
+//variable "db2_host_ip" {
+//  description = "IP address for the Db2"
+//}
+//
+//variable "db2_port_number" {
+//  description = "Port for Db2 instance"
+//}
+//
+variable "db2_standard_license_key" {
+  description = "The standard license key for the Db2 database product"
+}
+
+# DB2 Setting
 locals {
   cluster_config_path = ".kube/config"
-  entitled_registry_key_secret_name  = "ibm-entitlement-key"
-  docker_secret_name           = "docker-registry"
-  docker_server                = "cp.icr.io"
-  docker_username              = "cp"
-  docker_password              = chomp(var.entitlement_key)
-  docker_email                 = var.entitled_registry_user_email
-  project_name                 = "cp4ba"
 }
 
+locals {
+//  //  cp4ba_namespace              = "cp4ba"
+//  entitled_registry_key_secret_name = "ibm-entitlement-key"
+//  docker_secret_name                = "docker-registry"
+  docker_server                     = "cp.icr.io"
+  docker_username                   = "cp"
+//  docker_password                   = chomp(var.entitlement_key)
+//  docker_email                      = var.entitled_registry_user_email
+//
+//  enable_cluster = var.cluster_id == "" || var.cluster_id == null
+//  //  use_entitlement              = "yes"
+//  project_name     = "cp4ba"
+//  platform_options = 1     // 1: roks - 2: ocp - 3: private cloud
+//  deployment_type  = 2     // 1: demo - 2: enterprise
+//  platform_version = "4.6" // roks version
+
+  //  entitled_registry_key        = chomp(var.entitlement_key)
+//  ibmcloud_api_key = chomp(var.ibmcloud_api_key)
+}
