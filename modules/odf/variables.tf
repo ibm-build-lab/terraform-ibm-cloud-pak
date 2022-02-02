af07565c024253bd4314007575c658b0a36d0743
kosilva93 <kevinsilva93@hotmail.com>
2022-02-02 10:08:26 -0500

    Update variables.tf

--- modules/odf/variables.tf
+++ modules/odf/variables.tf
@@ -1,6 +1,6 @@
 variable "enable" {
     default     = true
-    description = "If set to true installs Portworx on the given cluster"
+    description = "If set to true installs ODF on the given cluster"
 }
 variable "kube_config_path" {
     description = "Path to the k8s config file: ex `~/.kube/config`"
