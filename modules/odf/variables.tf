b4407e99a85e0895218efcc701e924ca05758b75
kosilva93 <kevinsilva93@hotmail.com>
2022-02-07 13:56:15 -0500

    Update variables.tf

--- modules/odf/variables.tf
+++ modules/odf/variables.tf
@@ -6,10 +6,11 @@ variable "kube_config_path" {
     description = "Path to the k8s config file: ex `~/.kube/config`"
 }
 
-variable "cluster_id" {
+variable "cluster" {
   description = ""
 }
 
 variable "roks_version" {
+    default = "4.7"
   description = ""
 }
