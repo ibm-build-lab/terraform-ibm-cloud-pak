07b5e74c9138b884aa07085921d611266731f294
Eric Cambel <eric.cambel@ibm.com>
2022-02-18 15:49:41 -0500

    Added timeouts and checks

--- modules/cp4aiops/scripts/install_cp4aiops.sh
+++ modules/cp4aiops/scripts/install_cp4aiops.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # -----REMOVE---------
-# ON_VPC=false
+ON_VPC=false
+NAMESPACE="cp4aiops"
 # -----REMOVE---------
 
 K8s_CMD=kubectl
@@ -53,6 +54,9 @@ spec:
     enabled: false
 EOF
 
+### CHECK FOR INSTALLATION
+#oc get installations.orchestrator.aiops.ibm.com -n aiops
+
 
 echo "=== Adding pull secret to the ibm-aiops-orchestrator operator"
 
@@ -62,7 +66,7 @@ apiVersion: ai.ir.aiops.ibm.com/v1beta1
 kind: AIOpsAnalyticsOrchestrator
 metadata:
   name: aiops
-  namespace: aiops
+  namespace: $NAMESPACE
 spec:
   cassandra:
     bindingSecret: aiops-topology-cassandra-auth-secret
@@ -103,7 +107,80 @@ else
 fi
 
 
-# to find installation on cluster
-#oc get installations.orchestrator.aiops.ibm.com -n aiops
+# CHECK STATUS OF INSTALLATION
+is_complete_ircore=false
+is_complete_AIOpsAnalyticsOrchestrator=false
+is_complete_lifecycleservice=false
+is_complete_BaseUI=false
+is_complete_AIManager=false
+is_complete_aiopsedge=false
+is_complete_asm=false
+
+SLEEP_TIME="60" # seconds
+TIMEOUT_LIMIT=90 # 90min timout
+TIMEOUT_COUNT=0
+
+while true;
+  if [ "$TIMEOUT_COUNT" -eq "$TIMEOUT_LIMIT" ]; then
+        echo "=== problem installing custom resource, please check the operator log/events ==="
+        oc get installations.orchestrator.aiops.ibm.com -A &&
+          echo "" && oc get ircore,AIOpsAnalyticsOrchestrator -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.conditions[?(@.type==\"Ready\")].reason" &&
+          echo "" && oc get lifecycleservice -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.conditions[?(@.type==\"Lifecycle Service Ready\")].reason" &&
+          echo "" && oc get BaseUI -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.conditions[?(@.type==\"Ready\")].reason" &&
+          echo "" && oc get AIManager,aiopsedge,asm -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.phase"
+        exit 1
+    fi
+
+  if $is_complete_ircore && $is_complete_AIOpsAnalyticsOrchestrator && $is_complete_lifecycleservice && $is_complete_BaseUI && $is_complete_AIManager && $is_complete_aiopsedge && $is_complete_asm; then
+    break
+  fi
+
+  if [ $is_complete_ircore = false ] && [ `oc get ircore -A --output=json | jq -c -r '.items[].status.conditions[] | select( .type | contains( "Ready")) | .reason'` == "Ready"]; then
+    echo "ircore is ready"
+    is_complete_ircore=true
+  fi
+
+  if [ $is_complete_AIOpsAnalyticsOrchestrator = false ] && [ `oc get AIOpsAnalyticsOrchestrator -A --output=json | jq -c -r '.items[].status.conditions[] | select( .type | contains( "Ready")) | .reason'` == "Ready"]; then
+    echo "AIOpsAnalyticsOrchestrator is ready"
+    is_complete_AIOpsAnalyticsOrchestrator=true
+  fi
+
+  if [ $is_complete_lifecycleservice = false ] && [ `oc get lifecycleservice -A --output=json | jq -c -r '.items[].status.conditions[] | select( .type | contains( "Lifecycle Service Ready")) | .reason'` == "ready"]; then
+    echo "lifecycleservice is ready"
+    is_complete_lifecycleservice=true
+  fi
+
+  if [ $is_complete_BaseUI = false ] && [ `oc get BaseUI -A --output=json | jq -c -r '.items[].status.conditions[] | select( .type | contains( "Ready")) | .reason'` == "Ready"]; then
+    echo "BaseUI is ready"
+    is_complete_BaseUI=true
+  fi
+
+  if [ $is_complete_AIManager = false ] && [ `oc get AIManager -A --output=json | jq -c -r '.items[].status.phase'` == "Completed"]; then
+    echo "AIManager is ready"
+    is_complete_AIManager=true
+  fi
+
+  if [ $is_complete_aiopsedge = false ] && [ `oc get aiopsedge -A --output=json | jq -c -r '.items[].status.phase'` == "Configured"]; then
+    echo "aiopsedge is ready"
+    is_complete_aiopsedge=true
+  fi
+
+  if [ $is_complete_asm = false ] && [ `oc get asm -A --output=json | jq -c -r '.items[].status.phase'` == "OK"]; then
+    echo "asm is ready"
+    is_complete_asm=true
+  fi
+
+  # Every 10minutes, display status of installation
+  if (($TIMEOUT_COUNT % 10 == 0)); then
+    oc get installations.orchestrator.aiops.ibm.com -A &&
+      echo "" && oc get ircore,AIOpsAnalyticsOrchestrator -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.conditions[?(@.type==\"Ready\")].reason" &&
+      echo "" && oc get lifecycleservice -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.conditions[?(@.type==\"Lifecycle Service Ready\")].reason" &&
+      echo "" && oc get BaseUI -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.conditions[?(@.type==\"Ready\")].reason" &&
+      echo "" && oc get AIManager,aiopsedge,asm -A -o custom-columns="KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,STATUS:status.phase"
+  fi 
+
+  (( TIMEOUT_COUNT++ ))
+done
+
 
 echo '=== Installation Complete ==='
