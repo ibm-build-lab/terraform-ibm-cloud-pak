f271c78484ce4790bd7381b80a8e4aae6930f782
jgod360 <joel.goddot@gmail.com>
2022-02-23 11:49:40 -0500

    Update main.tf

--- examples/cp4aiops/main.tf
+++ examples/cp4aiops/main.tf
@@ -10,7 +10,7 @@ data "ibm_resource_group" "group" {
 resource "null_resource" "mkdir_kubeconfig_dir" {
   triggers = { always_run = timestamp() }
   provisioner "local-exec" {
-    command = "mkdir -p ${local.cluster_config_path}"
+    command = "mkdir -p ${var.cluster_config_path}"
   }
 }
 
@@ -18,7 +18,7 @@ data "ibm_container_cluster_config" "cluster_config" {
   depends_on = [null_resource.mkdir_kubeconfig_dir]
   cluster_name_id   = var.cluster_id
   resource_group_id = data.ibm_resource_group.group.id
-  config_dir        = local.cluster_config_path
+  config_dir        = var.cluster_config_path
 }
 
 module "cp4aiops" {
