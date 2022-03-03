56fa0544914ab9470c51cfd0593d197bbb4e9094
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-03-03 16:17:17 -0600

    Cleaned up module call

--- examples/odf/main.tf
+++ examples/odf/main.tf
@@ -1,17 +1,11 @@
 provider "ibm" {
-  region = var.region
 }
 
 // Module:
-
 module "odf" {
-  source = "./../../modules/odf"
-  // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using 'with_iaf' with 'count'
-  enable       = var.enable
-  cluster_id   = var.cluster_id
-  roks_version = var.roks_version
-
-  // Cluster parameters
-  //kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
-  kube_config_path = var.kube_config_path
+  source = "./.."
+  enable = var.enable
+  cluster = var.cluster
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
+
