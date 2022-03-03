47d78c7a669c80dd5c418cc1a5b988c999fa790b
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-03-03 16:18:08 -0600

    Removed unnecessary variables

--- examples/odf/vars.tf
+++ examples/odf/vars.tf
@@ -7,22 +7,6 @@ variable "ibmcloud_api_key" {
   description = "Get the ibmcloud api key from https://cloud.ibm.com/iam/apikeys"
 }
 
-variable "kube_config_path" {
-  description = "Path to the k8s config file: ex `~/.kube/config`"
-}
-
-variable "cluster_id" {
-  description = "The id of the cluster"
-}
-
-variable "region" {
-  description = "The region Portworx will be installed in: us-south, us-east, eu-gb, eu-de, jp-tok, au-syd, etc.."
-}
-
-variable "resource_group_name" {
-  description = "Resource Group in your account. List all available resource groups with: ibmcloud resource groups"
-}
-
-variable "roks_version" {
-  description = ""
+variable "cluster" {
+    description = "The id of the cluster"
 }
