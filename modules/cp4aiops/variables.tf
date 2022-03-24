0a5ed81e4658289bb4aeb66933d43110aff6721f
Eric Cambel <eric.cambel@ibm.com>
2022-03-24 13:50:24 -0400

    Fixed var file

--- modules/cp4aiops/variables.tf
+++ modules/cp4aiops/variables.tf
@@ -61,15 +61,7 @@ variable "enable_persistence" {
 }
 
 locals {
-<<<<<<< HEAD
   docker_registry          = "cp.icr.io" // Staging: "cp.stg.icr.io/cp/cpd"
   docker_username          = "cp"               // "ekey"
   entitled_registry_key    = chomp(var.entitlement_key)
-=======
-  docker_registry          = "cp.icr.io"
-  docker_username          = "cp"
-  entitled_registry_key    = chomp(var.entitled_registry_key)
-  openshift_version_regex  = regex("(\\d+).(\\d+)(.\\d+)*(_openshift)*", var.openshift_version)
-  openshift_version_number = local.openshift_version_regex[3] == "_openshift" ? tonumber("${local.openshift_version_regex[0]}.${local.openshift_version_regex[1]}") : 0
->>>>>>> d8cf41a117dd7034fcd02cfd07440a3cb11f6a0c
 }
