27bc9ff965c5fa9b90a037b2a50ad2e87a9b3b97
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-09-23 15:55:54 -0500

    added ip address

--- modules/Db2/outputs.tf
+++ modules/Db2/outputs.tf
@@ -2,19 +2,28 @@ output "db2_host_address" {
     depends_on = [
     data.external.get_endpoints,
   ]
-  description = "Use Host name of Db2 instance to update in  property \"db2_Host_address\" with this information (in Skytap, use the IP 10.0.0.10 instead."
-  value = var.enable_db2 && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.endpoint : ""
+  description = "Use Host name of Db2 instance to update in  property \"db2_Host_address\" with this information (in Skytap, use the IP 10.0.0.10 instead)"
+  value = var.enable_db2 && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.route : ""
 }
-
-output "db2_ports" {
+output "db2_ip_address" {
   depends_on = [
     data.external.get_endpoints,
   ]
-  description = "Use these Ports for Db2 instance to update in  property \"db2PortNumber\" with this information (legacy-server)."
-  value = var.enable_db2 && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.nodePort : ""
+  description = "Use this external IP address to reach DB2 service."
+  value = var.enable_db2 && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.ip_address : ""
 }
-
 output "db2_pod_name" {
+    depends_on = [
+    data.external.get_endpoints,
+  ]
   description = "This pod for deploying Db2 schemas."
   value = var.enable_db2 && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.db2_pod_name : ""
+}
+
+output "db2_ports" {
+    depends_on = [
+    data.external.get_endpoints,
+  ]
+  description = "The ports to reach DB2 instance"
+  value = var.enable_db2 && length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.nodePort : ""
 }
