54177603396d00cdc92effde0331153635050859
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-11-28 13:55:40 -0600

    Update variables.tf

--- odf/variables.tf
+++ odf/variables.tf
@@ -20,7 +20,7 @@ variable "osdStorageClassName" {
 variable "osdSize" {
   description = "Size of your storage devices. The total storage capacity of your ODF cluster is equivalent to the osdSize x 3 divided by the numOfOsd."
   type = string
-  default = "100Gi"
+  default = "200Gi"
 }
 
 variable "numOfOsd" {
